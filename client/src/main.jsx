ba647a61ad63cdb78f9df20453ffd602056e7086
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-12 00:17:44 -0500

    feat(main.jsx): Refactor main.jsx to include pages, router and createRoot

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+//Desc: Entry point of the app
+// ============================================================
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+// Importing libraries
+// ============================================================
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// ============================================================
+
+// Importing components
+// ============================================================
+import App from './App.jsx';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ErrorPage from './pages/ErrorPage';
+// ============================================================
+
+// Create a browser router
+// ============================================================
+const routes = [
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+            {
+                path: '/dashboard/:username',
+                element: <Dashboard />
+            },
+            {
+                path: '/profile/:username',
+                element: <Profile />
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+        ]
+    },
+];
+
+const router = createBrowserRouter(routes );
+// ============================================================
+
+// Create a root
+// ============================================================
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+// ============================================================
+
+// Render the app
+// ============================================================
+root.render(
+    <RouterProvider router={router} />
 );
+// ============================================================
