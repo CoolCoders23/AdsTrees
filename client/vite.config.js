cf250962e3df891bdcb5e4afee8fd399a60e54ce
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-12 02:59:56 -0500

    chore(vite): Update Vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// Desc: Vite config file
+// ===================================================
+
+// import libraries
+// ===================================================
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// ===================================================
 
 // https://vitejs.dev/config/
+// ===================================================
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port: 3000,
+        open: true,
+        proxy: {
+            '/graphql': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
+// ===================================================
