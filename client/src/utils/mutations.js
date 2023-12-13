1bad99cee17ca0ba4aec980e08cb061c9a1e1aaa
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-12 23:28:53 -0500

    feat(User): Add login and addUser mutations, and user query

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -5,3 +5,37 @@
 // ================================================================
 import { gql } from '@apollo/client';
 // ================================================================
+
+// Login the User
+// ================================================================
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+    {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+// ================================================================
+
+// Add a new User
+// ================================================================
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password)
+    {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+// ================================================================
