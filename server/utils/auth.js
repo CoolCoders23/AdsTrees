7d660a124979a08799894dcdebee784c16cd1993
Yega Cho <yega1009@gmail.com>
2023-12-13 17:29:47 -0500

    defined secret key and expiration time for jwt

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,3 +7,9 @@ const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 require('dotenv').config(); // Import and configure environment variables
 // ==================================================================
+
+// Define a secret key and set the expiration time for JWT
+// ==================================================================
+const secret = process.env.SECRET_KEY || 'mysecret';
+const expiration = Math.floor(Date.now() / 1000) + (60 * 60 * 2);
+// ==================================================================
