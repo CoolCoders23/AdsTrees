f92295e3672e60e7b337419bdea480d9d84342a7
Yega Cho <yega1009@gmail.com>
2023-12-13 17:29:17 -0500

    imported necessery dependencies

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,9 @@
 // Desc: This file contains the logic for the user Authentication.
 // ==================================================================
 
-// Dependencies
+// Import necessary dependencies
 // ==================================================================
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-// ==================================================================
-
-// define secret based on environment SECRET_KEY
-// ==================================================================
-const secret = process.env.SECRET_KEY;
-const expiration = Math.floor(Date.now() / 1000) + (60 * 60 * 2);// 2 hours
+require('dotenv').config(); // Import and configure environment variables
 // ==================================================================
