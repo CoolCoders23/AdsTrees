54b05ec1f6843d6044612bdd6483346ba1a029c1
Yega Cho <yega1009@gmail.com>
2023-12-13 17:30:17 -0500

    function to sign jwt token

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -13,3 +13,24 @@ require('dotenv').config(); // Import and configure environment variables
 const secret = process.env.SECRET_KEY || 'mysecret';
 const expiration = Math.floor(Date.now() / 1000) + (60 * 60 * 2);
 // ==================================================================
+
+module.exports = {
+    // Define a custom GraphQLError for authentication errors
+    // ==================================================================
+    AuthenticationError: new GraphQLError('Could not authenticate user.', {
+        extensions: {
+          code: 'UNAUTHENTICATED', // Custom error code indicating unauthenticated access
+        },
+    }),
+    // ==================================================================
+
+    // Function to sign a JWT token
+    // ==================================================================
+    signToken: function ({ username, email, _id }) {
+        // Prepare the payload with user data
+        const payload = { username, email, _id };
+        // Sign and return the JWT token with the user payload, secret, and expiration
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+    // ==================================================================
+};
