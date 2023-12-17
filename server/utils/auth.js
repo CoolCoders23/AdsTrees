dd2227798c2eb62d3457fc7f99d2995af2479d50
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-16 23:45:54 -0500

    feat(authServer): Add authentication error handling and middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,6 +4,7 @@
 // Dependencies
 // ==================================================================
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 require('dotenv').config();
 // ==================================================================
 
@@ -11,4 +12,43 @@ require('dotenv').config();
 // ==================================================================
 const secret = process.env.SECRET_KEY;
 const expiration = Math.floor(Date.now() / 1000) + (60 * 60 * 2);// 2 hours
+// ==================================================================
+
+// Exporting the Auth module
+// ==================================================================
+module.exports = {
+
+    AuthenticationError: new GraphQLError('Could not authenticate user.', {
+        extensions: {
+            code: 'UNAUTHENTICATED',
+        },
+    }),
+
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        return req;
+    },
+
+    signToken: function ({ email, username, _id }) {
+        const payload = { email, username, _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+
+};
 // ==================================================================
