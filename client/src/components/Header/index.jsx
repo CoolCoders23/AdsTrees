6a05d8d7ba93f5146e894b2e1e01e1d3f18f02b4
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-18 01:42:35 -0500

    fix: Refactor Header component and add theme support

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,4 +1,11 @@
 // Desc: This file contains the Header component of the app
+// Used the following as a reference:
+// https://chakra-ui.com/docs/layout/stack
+// https://chakra-ui.com/docs/layout/flex
+// https://chakra-ui.com/docs/getting-started
+// https://chakra-ui.com/docs/components
+// https://chakra-ui.com/docs/theming/theme
+// https://chakra-ui.com/docs/features/responsive-styles
 // ============================================================
 
 // Importing Dependencies
@@ -9,14 +16,21 @@ import {
     Button,
     Link,
     Text,
-    Stack
+    Stack,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Auth from '../../utils/auth';
 import logo from '../../assets/image/LogoMain/AdsTrees_Logo_64.svg';
+import { useTheme } from '../../utils/useTheme';
+// ============================================================
 
+// Create Header component
+// ============================================================
 const Header = () => {
+
+    const { theme, darkTheme } = useTheme();
+
     const location = useLocation();
     const path = location.pathname;
 
@@ -35,39 +49,115 @@ const Header = () => {
         console.error('Failed to get username', error);
     }
 
-    const NavLink = ({ to, children, isActive }) => (
-        <Button as={RouterLink} to={to} px={2} py={1} rounded={'md'} bg={isActive ? 'orange.500' : undefined} _hover={{ bg: 'gray.300' }} transition="0.3s">
-            <Text fontSize={['xs', 'sm', 'md', 'lg', 'xl']} fontFamily={'Arial'}>{children}</Text>
-        </Button>
-    );
+    const NavLink = ({ to, children, isActive }) => {
+
+        const colorMode = darkTheme ? 'dark' : 'light';
+        const buttonColor = colorMode === 'dark'
+            ? theme.colors.dark.greenMedium
+            : theme.colors.light.greenMedium;
+
+        return (
+            <Button
+                as={RouterLink}
+                to={to}
+                px={2}
+                py={1}
+                rounded={'md'}
+                bg={isActive ? undefined : 'transparent'}
+                transition="0.3s"
+                color={buttonColor}
+                variant="outline"
+            >
+                <Text
+                    fontSize={['xs', 'sm', 'md', 'lg', 'xl']}
+                    fontFamily={'Arial'}
+                >
+                    {children}
+                </Text>
+            </Button>
+        );
+    };
 
     const NavItems = () => (
-        <Stack direction={['column', 'row']} align="center" justify="space-between" spacing={4}>
-            <NavLink to='/about' isActive={path === '/about'}>About us</NavLink>
+
+        <Stack
+            direction={['column', 'row']}
+            align="center"
+            justify="space-between"
+            spacing={4}
+        >
+            <NavLink
+                to='/about'
+                isActive={path === '/about'}
+            >
+                About us
+            </NavLink>
+
             {Auth.loggedIn() && username !== '' ? (
                 <>
-                    <NavLink to={`/dashboard/${username}`} isActive={path.startsWith('/dashboard')}>Dashboard</NavLink>
-                    <NavLink to='/user-profile' isActive={path === '/user-profile'}>{username}&apos;s profile</NavLink>
-                    <Button as={RouterLink} to="/" onClick={logout}>Logout</Button>
+                    <NavLink
+                        to={`/dashboard/${username}`}
+                        isActive={path.startsWith('/dashboard')}
+                    >
+                        Dashboard
+                    </NavLink>
+                    <NavLink
+                        to='/user-profile'
+                        isActive={path === '/user-profile'}
+                    >
+                        {username}&apos;s profile
+                    </NavLink>
+                    <Button
+                        as={RouterLink}
+                        to="/" onClick={logout}
+                    >
+                        Logout
+                    </Button>
                 </>
             ) : (
-                path !== '/' && <NavLink to='/' isActive={path === '/'}>Login</NavLink>
+                path !== '/'
+                    &&
+                    <NavLink to='/' isActive={path === '/'}>
+                            Login
+                    </NavLink>
             )}
+
         </Stack>
     );
 
     return (
         <Box borderBottomWidth={1} mb={4} pb={2} p={4}>
-            <Flex align={'center'} justify={'space-between'} direction={['column', 'row']} spacing={4}>
+            <Flex
+                align={'center'}
+                justify={'space-between'}
+                direction={['column', 'row']}
+                spacing={4}
+            >
                 <Flex align={'center'}>
-                    <img src={logo} alt="logo" width="100" height="100" />
-                    <Link as={RouterLink} to='/' fontSize={['lg', 'xl']} fontWeight={'bold'} ml={2}>AdsTrees</Link>
+                    <img
+                        src={logo}
+                        alt="logo"
+                        width="100"
+                        height="100"
+                    />
+                    <Link
+                        as={RouterLink}
+                        to='/'
+                        fontSize={['lg', 'xl']}
+                        fontWeight={'bold'}
+                        ml={2}
+                    >
+                        AdsTrees
+                    </Link>
                 </Flex>
                 <NavItems />
             </Flex>
         </Box>
     );
 };
+// ============================================================
 
+// Exporting component
+// ============================================================
 export default Header;
 // ============================================================
