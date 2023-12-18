ce5f4d334bf635e7487ac95942167909879665f1
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-17 22:13:37 -0500

    style(header): Refactor Header component to use Chakra UI in order to style the header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -3,33 +3,28 @@
 
 // Importing Dependencies
 // ============================================================
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import {
+    Box,
+    Flex,
+    Button,
+    Link,
+    Text,
+    Stack
+} from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Auth from '../../utils/auth';
 import logo from '../../assets/image/LogoMain/AdsTrees_Logo_64.svg';
-// ============================================================
 
-// Define the Header component
-// ============================================================
 const Header = () => {
-    // Define the state for the navigation bar
-    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-
-    // Define the function to handle the toggling of the navigation bar
-    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
-
-    // Define the path
     const location = useLocation();
     const path = location.pathname;
 
-    // Define the function to handle the logout
     const logout = (event) => {
         event.preventDefault();
         Auth.logout();
     };
 
-    // Define the username
     let username = '';
     try {
         const profile = Auth.getProfile();
@@ -40,100 +35,39 @@ const Header = () => {
         console.error('Failed to get username', error);
     }
 
-    // Return the JSX for the Header component
+    const NavLink = ({ to, children, isActive }) => (
+        <Button as={RouterLink} to={to} px={2} py={1} rounded={'md'} bg={isActive ? 'orange.500' : undefined} _hover={{ bg: 'gray.300' }} transition="0.3s">
+            <Text fontSize={['xs', 'sm', 'md', 'lg', 'xl']} fontFamily={'Arial'}>{children}</Text>
+        </Button>
+    );
+
+    const NavItems = () => (
+        <Stack direction={['column', 'row']} align="center" justify="space-between" spacing={4}>
+            <NavLink to='/about' isActive={path === '/about'}>About us</NavLink>
+            {Auth.loggedIn() && username !== '' ? (
+                <>
+                    <NavLink to={`/dashboard/${username}`} isActive={path.startsWith('/dashboard')}>Dashboard</NavLink>
+                    <NavLink to='/user-profile' isActive={path === '/user-profile'}>{username}&apos;s profile</NavLink>
+                    <Button as={RouterLink} to="/" onClick={logout}>Logout</Button>
+                </>
+            ) : (
+                path !== '/' && <NavLink to='/' isActive={path === '/'}>Login</NavLink>
+            )}
+        </Stack>
+    );
+
     return (
-        <div>
-            <nav>
-                <div>
-                    <div>
-                        <img
-                            src={logo}
-                            alt="logo" width="100" height="100"
-                        />
-                        <Link to='/'>
-                            AdsTrees
-                        </Link>
-                        {/* TODO: add style to collapse */}
-                        <button
-                            type='button'
-                            aria-expanded={!isNavCollapsed ? true : false}
-                            aria-label='Toggle navigation'
-                            onClick={handleNavCollapse}
-                        >
-                            {/* TODO: toggler icon goes here */}
-                            <span></span>
-                        </button>
-                    </div>
-                    {/* TODO: fix this className */}
-                    <div
-                        className={`${isNavCollapsed ? '' : 'show'} navbar-collapse`}
-                        id='navbarNav'
-                    >
-                        <ul>
-                            <li>
-                                <Link
-                                    className={`nav-link ${
-                                        path === '/about' ? 'active' : 'text-dark'
-                                    }`}
-                                    to='/about'
-                                >
-                                    About us
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul>
-                            {Auth.loggedIn() && username !== '' ? (
-                                <>
-                                    <li>
-                                        <Link
-                                            className={`nav-link ${path.startsWith('/dashboard')
-                                                ? 'active'
-                                                : 'text-dark'
-                                            }`}
-                                            to={`/dashboard/${username}`}
-                                        >
-                                            Dashboard
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            className={`nav-link ${path === '/user-profile'
-                                                ? 'active'
-                                                : 'text-dark'
-                                            }`}
-                                            to='/user-profile'
-                                        >
-                                            {username}&apos;s profile
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/" onClick={logout}>
-                                            Logout
-                                        </Link>
-                                    </li>
-                                </>
-                            ) : (
-                                path !== '/' && (
-                                    <li>
-                                        <Link
-                                            className={`nav-link ${path === '/' ? 'active' : 'text-dark'}`}
-                                            to='/'
-                                        >
-                                            Login
-                                        </Link>
-                                    </li>
-                                )
-                            )}
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <Box borderBottomWidth={1} mb={4} pb={2} p={4}>
+            <Flex align={'center'} justify={'space-between'} direction={['column', 'row']} spacing={4}>
+                <Flex align={'center'}>
+                    <img src={logo} alt="logo" width="100" height="100" />
+                    <Link as={RouterLink} to='/' fontSize={['lg', 'xl']} fontWeight={'bold'} ml={2}>AdsTrees</Link>
+                </Flex>
+                <NavItems />
+            </Flex>
+        </Box>
     );
 };
-// ============================================================
 
-// Export the Header component
-// ============================================================
 export default Header;
 // ============================================================
--- client/src/utils/ThemeContext.jsx
+++ client/src/utils/ThemeContext.jsx
@@ -8,67 +8,13 @@
 // Importing libraries and packages
 // ============================================================
 import { createContext, useState } from 'react';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 // ============================================================
 
 // Create theme context
 // ============================================================
 export const ThemeContext = createContext();
-// ============================================================
-
-// Define the theme object to be used in the app
-// ============================================================
-const theme = extendTheme({
-    fonts: {
-        body: 'Roboto, system-ui, sans-serif',
-        heading: 'Poppins, Georgia, serif',
-        mono: 'Rubik Doodle Triangles, monospace',
-        coolTitle: 'Rubik Lines, monospace',
-    },
-    colors: {
-        light: {
-            orange: '#f5a61d',
-            greenLight: '#d8f3dc',
-            greenMedium: '#1b4332',
-            greenDark: '#081c15',
-            greenFlashy: '#9dffce',
-            greenPop: '#33c481',
-            pureWhite: '#ffffff',
-            brown: '#6d4c41',
-            greyLight: '#f7f9fb',
-            greyIntermediate: '#d9d9d9',
-            yellow: '#ffdc5f',
-            red: '#f15e2b',
-            blue: '#4285f4',
-            greenMidDark: '#415d43',
-            greyDark: '#49454f',
-            greenDarkTransparent: 'rgba(27, 67, 50, 0.10)',
-            purple: '#9747ff',
-        },
-        dark: {
-            orange: '#ffc662',
-            greenLight: '#d0ffd7',
-            greenMedium: '#269063',
-            greenDark: '#e8f5f1',
-            greenFlashy: '#9dffce',
-            greenPop: '#33c481',
-            whitePure: '#081c15',
-            brown: '#e29d86',
-            greyLight: 'rgba(180, 180, 180, 0.16)',
-            greyIntermediate: '#d9d9d9',
-            yellow: '#ffe999',
-            red: '#ff6a36',
-            blue: '#73a5f8',
-            greenMidDark: '#72bc77',
-            greyDark: '#f1f0f3',
-            greenDarkTransparent: 'rgba(116, 222, 177, 0.10)',
-            purple: '#af72ff',
-        },
-    },
-    shadows: {
-        dropShadowButtons: '0px 4px 61px 0px rgba(45, 106, 79, 0.40)',
-    },
-});
 // ============================================================
 
 // Creating theme provider
