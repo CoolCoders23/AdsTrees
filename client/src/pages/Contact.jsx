54eb0143b3eea6b53283d1222be937504ad9a3ee
Yega Cho <yega1009@gmail.com>
2023-12-17 19:14:02 -0500

    state for managing form data and messages

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -5,3 +5,19 @@ import emailjs from 'emailjs-com';
 import Auth from '../utils/auth';
 import { validateEmail } from '../utils/validateEmail';
 // ============================================================
+
+const Contact = () => {
+    // State for managing form data with initial values
+    // ============================================================
+    const [formData, setFormData] = useState({
+        user_name: '',
+        user_email: '',
+        message: ''
+    });
+    // State for managing messages to the user (like success or error messages)
+    const [message, setMessage] = useState('');
+    // ============================================================
+    };
+    // ============================================================
+
+export default Contact; // Exporting the Contact component
