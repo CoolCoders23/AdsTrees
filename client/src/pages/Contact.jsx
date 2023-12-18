da96c72ddaa3a07f234d2b847001973df9030a73
Yega Cho <yega1009@gmail.com>
2023-12-17 19:13:05 -0500

    imported necessary modules and utilities

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,7 @@
+// Importing useState hook from React and emailjs library
+// ============================================================
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
+import Auth from '../utils/auth';
+import { validateEmail } from '../utils/validateEmail';
+// ============================================================
