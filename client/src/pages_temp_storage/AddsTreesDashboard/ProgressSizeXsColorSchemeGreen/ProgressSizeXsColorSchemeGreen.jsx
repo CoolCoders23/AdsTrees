99059a65a276c8db9ab22c3e55e2a5a0824fdbf1
Shaduruk <jide@tidjani-serpos.com>
2023-12-22 08:19:54 -0500

    generate and import Profile React page from Figma

---
+++ client/src/pages_temp_storage/AddsTreesDashboard/ProgressSizeXsColorSchemeGreen/ProgressSizeXsColorSchemeGreen.jsx
@@ -0,0 +1,23 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import './ProgressSizeXsColorSchemeGreen.css';
+import { ProgressStripe } from '../ProgressStripe/ProgressStripe.jsx';
+
+export const ProgressSizeXsColorSchemeGreen = ({
+    hasStripe = true,
+    size = 'xs',
+    colorScheme = 'blue',
+    className,
+    ...props
+}) => {
+    return (
+        <div className={'progress-size-xs-color-scheme-green ' + className}>
+            <div className="inner">
+                {hasStripe && (
+                    <>
+                        <ProgressStripe className="stripe-instance" />
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
---
+++ client/src/pages_temp_storage/AdsTreesProfile/CheckboxControlSizeSmColorSchemeBlueIsCheckedTrueIsDisabledFalseIsIndeterminateFalse/CheckboxControlSizeSmColorSchemeBlueIsCheckedTrueIsDisabledFalseIsIndeterminateFalse.jsx
@@ -0,0 +1,28 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import './CheckboxControlSizeSmColorSchemeBlueIsCheckedTrueIsDisabledFalseIsIndeterminateFalse.css';
+
+export const CheckboxControlSizeSmColorSchemeBlueIsCheckedTrueIsDisabledFalseIsIndeterminateFalse =
+  ({ className, ...props }) => {
+      return (
+          <svg
+              className={
+                  'checkbox-control-size-sm-color-scheme-blue-is-checked-true-is-disabled-false-is-indeterminate-false ' +
+          className
+              }
+              width="12"
+              height="13"
+              viewBox="0 0 12 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+          >
+              <rect x="0.5" y="1" width="11" height="11" rx="1.5" fill="#33C481" />
+              <rect x="0.5" y="1" width="11" height="11" rx="1.5" stroke="#33C481" />
+              <path
+                  d="M2.46387 7.08846L4.7972 9.27596L9.46387 3.44263"
+                  stroke="#E8F5F1"
+                  strokeWidth="1.5"
+                  strokeLinejoin="round"
+              />
+          </svg>
+      );
+  };
