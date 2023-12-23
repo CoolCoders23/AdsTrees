485ed31919c7c0c5120b242518dba00caf6637a4
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-23 17:47:11 -0500

    feat(queries): Add queries for user purchases and donations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -19,6 +19,20 @@ export const QUERY_USER = gql`
         altText
       }
       preferences
+      purchases {
+        _id
+        purchaseDate
+        purchaseStatus
+        donations {
+          _id
+          donationType
+          description
+          donationAmount
+          price
+        }
+      }
+      totalDonations
+    }
   }
 `;
 // ================================================================
@@ -36,6 +50,86 @@ export const QUERY_USER_PROFILE = gql`
         altText
       }
       preferences
+      purchases {
+        _id
+        purchaseDate
+        purchaseStatus
+        donations {
+          _id
+          donationType
+          description
+          donationAmount
+          price
+        }
+      }
+      totalDonations
+    }
+  }
+`;
+// ================================================================
+
+// To query the user's donations
+// ================================================================
+export const QUERY_DONATIONS = gql`
+  query donations($donationType: String) {
+    donations(donationType: $donationType) {
+      _id
+      donationType
+      description
+      donationAmount
+      price
+    }
+  }
+`;
+// ================================================================
+
+// To query the user's purchases
+// ================================================================
+export const QUERY_PURCHASES = gql`
+  query purchases($donationType: String) {
+    purchases(donationType: $donationType) {
+      _id
+      purchaseDate
+      purchaseStatus
+      donations {
+        _id
+        donationType
+        description
+        donationAmount
+        price
+      }
+    }
+  }
+`;
+// ================================================================
+
+// To query a single purchase
+// ================================================================
+export const QUERY_PURCHASE = gql`
+  query purchase($_id: ID!) {
+    purchase(_id: $_id) {
+      _id
+      purchaseDate
+      purchaseStatus
+      donations {
+        _id
+        donationType
+        description
+        donationAmount
+        price
+      }
+    }
+  }
+`;
+// ================================================================
+
+// To query the checkout session
+// ================================================================
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($donations: [DonationInput]) {
+    checkout(donations: $donations) {
+      session
+    }
   }
 `;
 // ================================================================
