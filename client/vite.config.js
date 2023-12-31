3fa547fa79f2a1bda19a00bd6a9c8cea2c3358a3
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-31 15:34:29 -0500

    perf(vite.config): Add VitePWA plugin to vite.config.js, also add related configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,64 @@
 // ===================================================
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 // ===================================================
 
 // https://vitejs.dev/config/
 // ===================================================
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            // to be used only for development mode
+            devOptions: {
+                enabled: true
+            },
+            injectRegister: 'auto',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'AdsTrees',
+                short_name: 'AdsTrees',
+                description: 'AdsTrees',
+                theme_color: '#4CAF50',
+                background_color: '#ffffff',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ],
+            },
+        }),
+    ],
     server: {
         port: 3000,
         open: true,
