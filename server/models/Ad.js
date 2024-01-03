f6383557dab35214d0b4a9711b1731ec605ac90e
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-01-03 15:27:32 -0500

    feat: Add Ad model to server

---
+++ server/models/Ad.js
@@ -0,0 +1,50 @@
+// Desc: This file contains the model for Ads.
+// ==================================================================
+
+// Import Mongoose
+// ==================================================================
+const { Schema, model } = require('mongoose');
+// ==================================================================
+
+// Create the Ad Schema
+// ==================================================================
+const adSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: [3, 'Title must be at least 3 characters long.']
+    },
+
+    watched: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+
+    // Duration in seconds
+    duration: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
+// ==================================================================
+
+// Create the Ad model using the Ad Schema
+// ==================================================================
+const Ad = model('Ad', adSchema);
+// ==================================================================
+
+// Export the Ad model
+// ==================================================================
+module.exports = Ad;
+// ==================================================================
