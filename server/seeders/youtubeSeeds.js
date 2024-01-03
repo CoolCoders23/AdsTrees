8dd8f577b15d7876f92caa06baa2a68f367c6b89
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-01-03 17:42:31 -0500

    feat: Add Youtube video seed data

---
+++ server/seeders/youtubeSeeds.js
@@ -0,0 +1,103 @@
+// Desc: this file contains the model for Youtube videos.
+// ==================================================================
+
+// Define the Youtube Seeder
+// ==================================================================
+const youtubeData = [
+    {
+        title: 'Technology Ad One',
+        url: 'https://www.youtube.com/watch?v=XHTrLYShBRQ',
+        duration: 47,
+    },
+    {
+        title: 'Technology Ad Two',
+        url: 'https://www.youtube.com/watch?v=9TwvDZxc530',
+        duration: 30,
+    },
+    {
+        title: 'Fashion and Beauty Ad One',
+        url: 'https://www.youtube.com/watch?v=RRv5udKGG68',
+        duration: 30,
+    },
+    {
+        title: 'Fashion and Beauty Ad Two',
+        url: 'https://www.youtube.com/watch?v=pOe5M0GtYZo',
+        duration: 30,
+    },
+    {
+        title: 'Home and Garden Ad One',
+        url: 'https://www.youtube.com/watch?v=bLbiUqIu8WE',
+        duration: 30,
+    },
+    {
+        title: 'Home and Garden Ad Two',
+        url: 'https://www.youtube.com/watch?v=fkhb5mhQf18',
+        duration: 30,
+    },
+    {
+        title: 'Travel and Tourism Ad One',
+        url: 'https://www.youtube.com/watch?v=eAtXWHPlykY',
+        duration: 30,
+    },
+    {
+        title: 'Travel and Tourism Ad Two',
+        url: 'https://www.youtube.com/watch?v=kACrKtpV9Kk',
+        duration: 30,
+    },
+    {
+        title: 'Food and Beverages Ad One',
+        url: 'https://www.youtube.com/watch?v=sAar3Xlarz8',
+        duration: 30,
+    },
+    {
+        title: 'Food and Beverages Ad Two',
+        url: 'https://www.youtube.com/watch?v=9cPxh2DikIA',
+        duration: 30,
+    },
+    {
+        title: 'Financial Services Ad One',
+        url: 'https://www.youtube.com/watch?v=ZXeiZIyx2JE',
+        duration: 30,
+    },
+    {
+        title: 'Financial Services Ad Two',
+        url: 'https://www.youtube.com/watch?v=gf1SakItQXw',
+        duration: 30,
+    },
+    {
+        title: 'Education and Learning Ad One',
+        url: 'https://www.youtube.com/watch?v=qNSVb3gJOdQ',
+        duration: 30,
+    },
+    {
+        title: 'Education and Learning Ad Two',
+        url: 'https://www.youtube.com/watch?v=nU9qco_m4gM',
+        duration: 58,
+    },
+    {
+        title: 'Entertainment and Media Ad One',
+        url: 'https://www.youtube.com/watch?v=ds3lFKNSO5A',
+        duration: 30,
+    },
+    {
+        title: 'Entertainment and Media Ad Two',
+        url: 'https://www.youtube.com/watch?v=7JU-atQOLUw',
+        duration: 45,
+    },
+    {
+        title: 'Automotive Industry Ad One',
+        url: 'https://www.youtube.com/watch?v=UJWz0dl6S1A',
+        duration: 35,
+    },
+    {
+        title: 'Automotive Industry Ad Two',
+        url: 'https://www.youtube.com/watch?v=X7L_K0-aJe4',
+        duration: 65,
+    }
+];
+// ==================================================================
+
+// Export the Youtube Seeder
+// ==================================================================
+module.exports = youtubeData;
+// ==================================================================
