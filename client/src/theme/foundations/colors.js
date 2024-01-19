3422a455ae9d242c0839310bc0c012d08afc8cec
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-01-19 18:21:31 -0500

    perf: Add foundational style overrides for colors

---
+++ client/src/theme/foundations/colors.js
@@ -0,0 +1,80 @@
+// Desc: This file contains the foundational style overrides for colors
+// ============================================================
+
+// Define Overrides
+// ============================================================
+const colors = {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#fff', //constantWhitePure-whitePure-cardsWhite
+    // Green palate for light mode(from light to dark)
+    greenLM:{
+        50: '#c5edcb', //greenLight
+        100: '#b3ffd9', //greenFlashy
+        200: '#a6e1be',
+        300: '#81d5a6',
+        400: '#60ca90',
+        500: '#2fb678', //greenPop-tag
+        600: '#49694c',//greenMidDark
+        700: '#1637291a', //greenDarkTransparent
+        800: '#163729', //greenMedium
+        900: '#06140f', //constantGreenDark-greenDark
+        //cardsMainColor- stepperGreen-cardsWhite-whitePure
+    },
+    // Green palate for dark mode(from dark to light)
+    greenDM: {
+        50: '#1f2d29', //videoCover
+        100: '#258d61', //greenMedium
+        200: '#2fb678', //greenPop
+        300: '#63b569', //greenMidLight
+        400: '#b3ffd9', //stepperGreen
+        500: '#b3ffd9', //greenFlashy
+        600: '#6fddae1a', //greenDarkTransparent
+        700: '#e5ffe9', //greenLight-tag
+        800: '#ecf8f5', //cardsMainColor
+        900: '#edf7f4', //greenDark
+    },
+    // Grey palate for light mode(from light to dark)
+    greyLM: {
+        50: '#eef2f7', //greyLight
+        100: '#d7d6dd',
+        200: '#d8d9d8', //videoCover
+        300: '#d9d9d9', //greyIntermediate
+        400: '#898492',
+        500: '#706b79',
+        600: '#57535e',
+        700: '#3f3b44', //greyDark
+        800: '#26242b',
+        900: '#0e0a14',
+    },
+    // Grey palate for dark mode(from dark to light)
+    greyDM: {
+        700: '#d9d9d9', //greyIntermediate
+        800: '#bfbfbf29',// greyLight
+        900: '#f2f1f4', //greyDark
+    },
+    // light mode colors
+    lM: {
+        orange: '#f5a61d',
+        brown: '#6d4c41',
+        yellow: '#ffdc5f',
+        red: '#cf3535',
+        blue: '#4285f4',
+        purple: '#9747ff',
+    },
+    // dark mode colors
+    dM: {
+        orange: '#ffc662',
+        brown: '#e29d86',
+        yellow: '#ffe999',
+        red: '#f53636',
+        blue: '#73a5f8',
+        purple: '#af72ff',
+    },
+};
+// ============================================================
+
+// Export Overrides
+// ============================================================
+export default colors;
+// ============================================================
