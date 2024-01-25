bc65da41071096c923f2290f77b3003b907b8837
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-01-25 00:51:06 -0500

    chore: Update styles.js with header and footer styles

--- client/src/theme/styles.js
+++ client/src/theme/styles.js
@@ -14,26 +14,33 @@ const styles = {
             color: mode('light.text', 'dark.text')(props),
             bg: mode('light.primary', 'dark.primary')(props),
         },
+        header: {
+            boxShadow: props.colorMode === 'light' ? '0px 1px 5px rgba(0, 0, 0, 0.1)' : '0px 1px 5px rgba(255, 255, 255, 0.1)',
+        },
+        '.footer': {
+            boxShadow: props.colorMode === 'light' ? '0px 1px 5px rgba(0, 0, 0, 0.1)' : '0px 1px 5px rgba(255, 255, 255, 0.1)',
+        },
         a: {
             textDecoration: 'none',
         },
-        'input': {
+        'textarea, .input-group2': {
             color: mode('light.inputText', 'dark.inputText')(props),
-        },
-        'input::placeholder': {
-            color: mode('light.inputText', 'dark.inputText')(props),
-        },
-        'input:-ms-input-placeholder': {
-            color: mode('light.inputText', 'dark.inputText')(props),
+            backgroundColor: mode('light.inputBackground', 'dark.inputBackground')(props),
+            borderColor: mode('light.inputBorder', 'dark.inputBorder')(props),
+            borderStyle: 'solid',
+            borderWidth: '2px',
+            borderRadius: '10px',
+            _focus: {
+                outline: 'none',
+                boxShadow: 'none',
+                borderColor: mode('light.inputFocusBorder', 'dark.inputFocusBorder')(props),
+            }
         },
         'button div, button span, button p, button': {
             color: mode('light.buttonText', 'dark.buttonText')(props),
         },
         '.error': {
             color: mode('light.errorText', 'dark.errorText')(props),
-        },
-        'textarea': {
-            color: mode('light.inputText', 'dark.inputText')(props),
         },
     }),
 };
