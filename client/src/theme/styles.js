4ab02081c4cb188479d053b56bfe3625b0a8443b
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-01-26 01:49:41 -0500

    perf: Update global styles in theme

--- client/src/theme/styles.js
+++ client/src/theme/styles.js
@@ -10,19 +10,24 @@ import { mode } from '@chakra-ui/theme-tools';
 // ============================================================
 const styles = {
     global: (props) => ({
+
         body: {
             color: mode('light.text', 'dark.text')(props),
             bg: mode('light.primary', 'dark.primary')(props),
         },
+
         header: {
             boxShadow: props.colorMode === 'light' ? '0px 1px 5px rgba(0, 0, 0, 0.1)' : '0px 1px 5px rgba(255, 255, 255, 0.1)',
         },
+
         '.footer': {
             boxShadow: props.colorMode === 'light' ? '0px 1px 5px rgba(0, 0, 0, 0.1)' : '0px 1px 5px rgba(255, 255, 255, 0.1)',
         },
+
         a: {
             textDecoration: 'none',
         },
+
         'textarea, .input-group2': {
             color: mode('light.inputText', 'dark.inputText')(props),
             backgroundColor: mode('light.inputBackground', 'dark.inputBackground')(props),
@@ -36,12 +41,51 @@ const styles = {
                 borderColor: mode('light.inputFocusBorder', 'dark.inputFocusBorder')(props),
             }
         },
-        'button div, button span, button p, button': {
-            color: mode('light.buttonText', 'dark.buttonText')(props),
-        },
+
         '.error': {
             color: mode('light.errorText', 'dark.errorText')(props),
         },
+
+        '.client-controllers-frame': {
+            bg: mode('light.primary', 'dark.primary')(props),
+        },
+
+        '.stats-figure-card-instance, .bonus-card-instance': {
+            bg: mode('light.dashboardCardBg', 'dark.dashboardCardBg')(props),
+        },
+
+        '.pricing-card ': {
+            bg: mode('light.priceCardBg', 'dark.primary')(props),
+            borderColor: mode('light.priceCardBorder', 'dark.priceCardBorder')(props),
+            boxShadow: mode('0px 2px 12px 0px rgba(45, 106, 79, 0.4)', '0px 2px 12px 0px rgba(45, 106, 79, 0.4)')(props),
+        },
+
+        '.svgFill': {
+            fill: mode('light.teamImageBg', 'dark.greenFlashy')(props),
+        },
+        '.team-member-data-frame': {
+            bg: mode('dark.greenFlashy', 'light.teamImageBg')(props),
+        },
+        '.aboutIconsFill': {
+            fill: mode('light.text', 'dark.text')(props),
+        },
+
+        '.active-line, .dot': {
+            bg: mode('light.greenDark', 'dark.greenFlashy')(props),
+        },
+        '.inactive-line': {
+            bg: mode('light.inactiveLine', 'dark.greyIntermediate')(props),
+        },
+        '.welcome': {
+            color: mode('light.greenDark', 'dark.greenFlashy')(props),
+        },
+        '.bbackground, .step-icon-base, .step-icon-base2 ': {
+            bg: mode('light.bbackground', 'dark.bbackground')(props),
+        },
+        '.payment, .confirmation': {
+            color: mode('light.bbackground', 'dark.text')(props),
+        },
+
     }),
 };
 // ============================================================
